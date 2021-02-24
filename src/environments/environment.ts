55fe05fe6db188ddb4a1f31337f3ccb0ea3e80ff
Klaudia Salillari <klaudia.salillari@atis.al>
2021-02-24 23:54:40 +0100

    initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,24 @@
 
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyBH2EwcMrhydxiw9z2NE7u92EiqAxZQ-Rg",
-    authDomain: "ionic-booking-93ce3.firebaseapp.com",
-    databaseURL: "https://ionic-booking-93ce3.firebaseio.com",
-    projectId: "ionic-booking-93ce3",
-    storageBucket: "ionic-booking-93ce3.appspot.com",
-    messagingSenderId: "316457555",
-    appId: "1:316457555:web:08035fb7d64cfa3503fca6",
-    measurementId: "G-2DQ1ME1LXP"
+  // firebaseConfig: {
+  //   apiKey: "AIzaSyBH2EwcMrhydxiw9z2NE7u92EiqAxZQ-Rg",
+  //   authDomain: "ionic-booking-93ce3.firebaseapp.com",
+  //   databaseURL: "https://ionic-booking-93ce3.firebaseio.com",
+  //   projectId: "ionic-booking-93ce3",
+  //   storageBucket: "ionic-booking-93ce3.appspot.com",
+  //   messagingSenderId: "316457555",
+  //   appId: "1:316457555:web:08035fb7d64cfa3503fca6",
+  //   measurementId: "G-2DQ1ME1LXP"
+  // }
+   firebaseConfig : {
+    apiKey: "AIzaSyAw-v3drkN5boytU4ndhvuYQWZtdPSER0M",
+    authDomain: "parkingapp-e0199.firebaseapp.com",
+    projectId: "parkingapp-e0199",
+    storageBucket: "parkingapp-e0199.appspot.com",
+    messagingSenderId: "861604177518",
+    appId: "1:861604177518:web:3b19108b7cc7a896fbe87f",
+    measurementId: "G-55JFCFVZ7Q"
   }
 };
 /*
