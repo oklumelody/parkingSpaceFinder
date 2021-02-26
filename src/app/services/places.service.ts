4b94d58309691942b672c8a6de7de06ee2b2a7e6
klaudia-atis <klaudiasalillari@hotmail.com>
2021-02-26 16:28:35 +0100

    new modif

--- src/app/services/places.service.ts
+++ src/app/services/places.service.ts
@@ -12,30 +12,30 @@ export class PlacesService {
     [
       {
         id: '1',
-        title: 'Manhattan Mansion',
-        description: 'In the heart of New York City',
-        imageUrl: 'https://via.placeholder.com/400',
-        price: 149.99,
+        title: 'Parkim me pagese',
+        description: 'Rruga Vace Zela - 100ALL/hour',
+        imageUrl: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fwallup.net%2Fwp-content%2Fuploads%2F2018%2F09%2F25%2F599350-Cesar_Bazkez-parking_lot-garages-architecture-748x463.jpg&imgrefurl=https%3A%2F%2Fwallup.net%2Fcesar-bazkez-parking-lot-garages-architecture%2F&tbnid=mZsBXZEgrm2miM&vet=12ahUKEwji-pas7ofvAhUQP-wKHQk9AycQMygaegUIARD9AQ..i&docid=4y4YCuDMxQjf4M&w=748&h=463&q=parking%20photo&client=ubuntu&ved=2ahUKEwji-pas7ofvAhUQP-wKHQk9AycQMygaegUIARD9AQ',
+        price: 1,
         availableFrom: new Date('2019-01-01'),
         availableTo: new Date('2019-01-01'),
         userId: 'abc'
       },
       {
         id: '2',
-        title: 'Manhattan Mansion',
-        description: 'In the heart of New York City',
-        imageUrl: 'https://via.placeholder.com/400',
-        price: 149.99,
+        title: 'Parkim me pagese',
+        description: 'Rruga Ibrahim Rugova - 100ALL/hour',
+        imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWwOdIeKj8T3k05C_SBdsnWDaYkcTitZXs4g&usqp=CAU',
+        price: 1,
         availableFrom: new Date('2019-01-01'),
         availableTo: new Date('2019-01-01'),
         userId: 'abc'
       },
       {
         id: '3',
-        title: 'Manhattan Mansion',
-        description: 'In the heart of New York City',
-        imageUrl: 'https://via.placeholder.com/400',
-        price: 149.99,
+        title: 'Parkim me pagese',
+        description: 'Blv Zogu I - 150ALL/hour',
+        imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmeP8InDZ_veSgbNelew_k72gvfO_eHEgyWA&usqp=CAU',
+        price: 1.5,
         availableFrom: new Date('2019-01-01'),
         availableTo: new Date('2019-01-01'),
         userId: 'abc'
